03bd75eb35e08c883197df35c8d813dfab1e3fb7
prabin738 <prabinsingh750@gmail.com>
2025-08-26 12:45:45 +0545

    basic design finished and now working with api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
 import React from "react";
+import Category from "./components/Category";
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <>
+      <div className="py-[40px]">
+        <div className="max-w- [1320px] mx-auto ">
+          <h1 className="text-center text-[40px] font-bold mb-[30px]  ">
+            Our Products
+          </h1>
+          <div className="grid frid-cols-[30%_auto] gap-[20px] ">
+            <div>
+              <Category />
+            </div>
+            <div>
+              <div className="grid grid-cols-3 gap-4">
+                <ProductItems />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
 export default App;
+
+function ProductItems() {
+  return (
+    <div className="shadow-lg text-center pb-4 ">
+      <img
+        src="https://yuvaanjewels.com/wp-content/uploads/2025/08/IMG_20250812_172809_794.jpg"
+        alt=""
+      />
+      <h4>Bracelet</h4>
+      <b>Rs 1000</b>
+    </div>
+  );
+}
---
+++ src/components/Category.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Category() {
+  return (
+    <div>
+      <h3 className="text-[25px] font-[500] p-[10px] "> Product Category</h3>
+
+      <ul>
+        <li className=" bg-[#ccc] p-[7px] crusor-pointer text-[20px] font-serif font-[500] mb-2 ">
+          Braccelet
+        </li>
+      </ul>
+    </div>
+  );
+}
