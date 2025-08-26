1ebb196728c38135b343c367d1d0186b0ffbb962
prabin738 <prabinsingh750@gmail.com>
2025-08-26 13:58:32 +0545

    api data fetced for both categories and product

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Category from "./components/Category";
+import axios from "axios";
 
 const App = () => {
+  //objects are array that's why we are passing [] in usestate
+  let [finalCategory, setFinalCategory] = useState([]);
+  let [finalProduct, setFinalProduct] = useState([]);
+
+  //function to get product categories
+  let getCategory = () => {
+    axios
+      .get("https://dummyjson.com/products/categories")
+      .then((res) => res.data)
+      .then((finalRes) => {
+        setFinalCategory(finalRes);
+      })
+      .catch((err) => {
+        console.log("Error fetching categories", err);
+      });
+  };
+
+  //function to get product
+
+  let getProducts = () => {
+    axios
+      .get("https://dummyjson.com/products")
+      .then((proRes) => proRes.data)
+      .then((finalRes) => {
+        setFinalProduct(finalRes.products);
+        //products is from that api
+      });
+  };
+
+  useEffect(() => {
+    getCategory();
+    getProducts();
+  }, []);
+
+  let Pitems = finalProduct.map((products, index) => {
+    return <ProductItems key={index} pdata={products} />;
+  });
+
   return (
     <>
       <div className="py-[40px]">
-        <div className="max-w- [1320px] mx-auto ">
+        <div className="max-w-[1320px] mx-auto ">
           <h1 className="text-center text-[40px] font-bold mb-[30px]  ">
             Our Products
           </h1>
-          <div className="grid frid-cols-[30%_auto] gap-[20px] ">
+          <div className="grid grid-cols-[30%_auto] gap-[20px] ">
             <div>
-              <Category />
+              {/* {finalCategory.length} test data is fetched or not */}
+              <Category finalCategory={finalCategory} />
             </div>
             <div>
-              <div className="grid grid-cols-3 gap-4">
-                <ProductItems />
-              </div>
+              <div className="grid grid-cols-3 gap-4">{Pitems}</div>
             </div>
           </div>
         </div>
@@ -27,15 +65,16 @@ const App = () => {
 
 export default App;
 
-function ProductItems() {
+function ProductItems({ pdata }) {
+  console.log(pdata);
   return (
     <div className="shadow-lg text-center pb-4 ">
       <img
         src="https://yuvaanjewels.com/wp-content/uploads/2025/08/IMG_20250812_172809_794.jpg"
         alt=""
       />
-      <h4>Bracelet</h4>
-      <b>Rs 1000</b>
+      <h4>{pdata.title}</h4>
+      <b>$ {pdata.price}</b>
     </div>
   );
 }
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 
-export default function Category() {
+export default function Category({ finalCategory }) {
+  //run data in loop
+
+  let cat = finalCategory.map((v, i) => {
+    return (
+      <li
+        key={i}
+        className="bg-[#ccc] p-[7px] cursor-pointer text-[20px] font-serif font-[500] mb-2"
+      >
+        {v.name}
+      </li>
+    );
+  });
+
   return (
     <div>
       <h3 className="text-[25px] font-[500] p-[10px] "> Product Category</h3>
 
-      <ul>
-        <li className=" bg-[#ccc] p-[7px] crusor-pointer text-[20px] font-serif font-[500] mb-2 ">
-          Braccelet
-        </li>
-      </ul>
+      <ul>{cat}</ul>
     </div>
   );
 }
